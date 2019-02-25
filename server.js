83403e2f7ae1f02dfc193b913463e0bf78dbfd60
Patryk Kozarski <Patryknerio2@gmail.com>
2019-02-25 18:29:44 +0100

    /names done

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const jsonfile = require('jsonfile');
-const port=11290;
+const port = 11290;
 const app = express();
 
 app.use(morgan('combined'));
@@ -10,17 +10,31 @@ app.use(cors());
 
 const file = jsonfile.readFileSync("Poland_names.json");
 
-//console.log(json[1].Rok);
-
 app.get('/names', (req, res) => {
-    let names=[];
-    file.forEach( (obj)=>{
-        if(names.indexOf(obj.Imie)==-1)
-            names.push(obj.Imie);
+    const start = parseInt(req.query.start);
+    const limit = parseInt(req.query.limit);
+    const year = parseInt(req.query.year);
+    let skip = false;
+    let years = true;
+    if (isNaN(start) || isNaN(limit))
+        skip = true;
+    if (isNaN(year))
+        years = false;
+    let names = [];
+    file.forEach((obj) => {
+        if (names.indexOf(obj.Imie) === -1)
+            if (years) {
+                if (obj.Rok === year)
+                    names.push(obj.Imie);
+            } else
+                names.push(obj.Imie);
+        if (!skip && names.length > start + limit)
+            return 0;
     });
-    res.json(names);
+    const response = skip ? names : names.slice(start, start + limit);
+    res.json(response);
 });
 
 app.listen(port, () => {
-    console.log("app listen on "+port);
+    console.log("app listen on " + port);
 });
